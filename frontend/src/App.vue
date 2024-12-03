35cf95d48358ae2112b86d1023753b09ca90e40d
AmadoMuerte <kisik854@gmail.com>
2024-12-03 21:27:21 +1000

    create vue app

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const socket = new WebSocket('ws://localhost:8080/api/v1/websocket/metrics'); 
+socket.onopen = (event) => {
+  console.log("WebSocket connection opened");
+};
+
+socket.onmessage = (event) => {
+  console.log("WebSocket message received:", JSON.parse(event.data));
+  socket.close()
+};
+
+</script>
+
+<template>
+  
+</template>
